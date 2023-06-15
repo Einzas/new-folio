28f70993795536d6733a4ba50907d875474ff9fb
Jeimy Jara <jeimyjara@hotmail.com>
2023-06-15 18:30:37 -0500

    Icon fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
       </header>
       <nav>
         <ul className="grid gap-8 sm:text-2xl text-center">
-          <li className="expand duration-300 hover:bg-blue-500 py-2 px-3 transition-colors duration-300">
+          <li className="expand  hover:bg-blue-500 py-2 px-3 transition-colors duration-300">
             <Link to={"/about"}>About</Link>
           </li>
           <li className="expand duration-300">
